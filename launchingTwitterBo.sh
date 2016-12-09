3622d83903078ed637d04c06ea061e3f7c911383
Sébastien de Beauffort <sewelseb@hotmail.com>
2016-12-09 14:33:10 +0100

    bot.sh

--- launchingTwitterBo.sh
+++ launchingTwitterBo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 #envoie une réponse aux gent qui ont ajouté une vidéo de paintball dans leur playlist
-php main.php
+#php main.php
+
+#pour le serveur de simon
+docker exec -i PHP-CMO  php /var/www/html/botCMO/main.php
